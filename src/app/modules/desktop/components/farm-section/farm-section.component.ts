b59861cd01705de657813498246d85b1f37a3efc
Ewerton Mendes <ewerton.mendes@terceirizadoraizen.com>
2024-07-11 19:51:19 -0300

    chore: adds digimon lists on farm section

--- src/app/modules/desktop/components/farm-section/farm-section.component.ts
+++ src/app/modules/desktop/components/farm-section/farm-section.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { Digimon } from '../../../../core/interfaces/digimon.interface';
+import { DigiStatusCardComponent } from '../../../../shared/components/digi-status-card/digi-status-card.component';
 
 @Component({
   selector: 'app-farm-section',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, DigiStatusCardComponent],
   templateUrl: './farm-section.component.html',
   styleUrl: './farm-section.component.scss'
 })
 export class FarmSectionComponent {
-
+  inTrainingDigimonList: Digimon[] = [
+    { name: 'Greymon', img: '/assets/digimons/Greymon.webp', hp: 100, mp: 50, atk: 30, def: 20, exp: 0, level: 1 },
+    { name: 'Greymon', img: '/assets/digimons/Greymon.webp', hp: 100, mp: 50, atk: 30, def: 20, exp: 0, level: 1 },
+    { name: 'Greymon', img: '/assets/digimons/Greymon.webp', hp: 100, mp: 50, atk: 30, def: 20, exp: 0, level: 1 },
+    { name: 'Greymon', img: '/assets/digimons/Greymon.webp', hp: 100, mp: 50, atk: 30, def: 20, exp: 0, level: 1 },
+  ]
 }

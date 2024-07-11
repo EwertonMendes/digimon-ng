4b3d06fd23e212af2e4784326de66450245403af
Ewerton Mendes <ewerton.mendes@terceirizadoraizen.com>
2024-07-11 18:28:53 -0300

    feat: adds initial structure for farm section

---
+++ src/app/modules/desktop/components/farm-section/farm-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+
+@Component({
+  selector: 'app-farm-section',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './farm-section.component.html',
+  styleUrl: './farm-section.component.scss'
+})
+export class FarmSectionComponent {
+
+}
--- src/app/modules/desktop/desktop.component.ts
+++ src/app/modules/desktop/desktop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeSectionComponent } from './components/home-section/home-section.component';
+import { FarmSectionComponent } from './components/farm-section/farm-section.component';
 
 @Component({
   selector: 'app-desktop',
   standalone: true,
-  imports: [HomeSectionComponent],
+  imports: [HomeSectionComponent, FarmSectionComponent],
   templateUrl: './desktop.component.html',
   styleUrl: './desktop.component.scss'
 })

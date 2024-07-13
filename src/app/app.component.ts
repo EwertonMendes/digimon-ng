45cda683d585c757481949500908860053180a17
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-07-13 19:29:42 -0300

    chore: adds action bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DesktopComponent } from './modules/desktop/desktop.component';
 import { GlobalStateDataSource } from './global-state.datasource';
+import { ButtonComponent } from "./shared/components/button/button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DesktopComponent],
+  imports: [RouterOutlet, DesktopComponent, ButtonComponent],
   providers: [GlobalStateDataSource],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

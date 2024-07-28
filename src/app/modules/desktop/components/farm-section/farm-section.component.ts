87a4bf72c33d40feb4db022347e1463aa3644180
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-07-28 15:36:11 -0300

    refactor: improves code quality on drop method inside farm section

--- src/app/modules/desktop/components/farm-section/farm-section.component.ts
+++ src/app/modules/desktop/components/farm-section/farm-section.component.ts
@@ -64,7 +64,10 @@ export class FarmSectionComponent {
 
   removeDigimonFromTraining(event: MouseEvent, digimon: Digimon): void {
     event.preventDefault();
-    this.globalState.addDigimonToStorage(digimon, DigimonListLocation.IN_TRAINING);
+    this.globalState.addDigimonToStorage(
+      digimon,
+      DigimonListLocation.IN_TRAINING
+    );
   }
 
   removeDigimonFromFarm(event: MouseEvent, digimon: Digimon): void {
@@ -78,57 +81,107 @@ export class FarmSectionComponent {
   }
 
   drop(event: CdkDragDrop<any>) {
-    if (event.previousContainer === event.container) {
-      if (event.container.id === this.inTrainingListId) {
-        moveItemInArray(
-          this.globalState.playerDataAcessor.inTrainingDigimonList,
-          event.previousIndex,
-          event.currentIndex
-        );
+    const { previousContainer, container, previousIndex, currentIndex } = event;
 
-        return;
-      }
-
-      if (event.container.id === this.bitFarmingListId) {
-        moveItemInArray(
-          this.globalState.playerDataAcessor.bitFarmDigimonList,
-          event.previousIndex,
-          event.currentIndex
-        );
-        return;
-      }
+    if (previousContainer === container) {
+      this.handleSameContainerDrop(container.id, previousIndex, currentIndex);
       return;
     }
 
-    if (event.previousContainer.id === this.inTrainingListId) {
-      this.globalState.addDigimonToFarm(
-        event.previousContainer.data.inTrainingDigimonList[event.previousIndex],
-        this.actions[event.previousContainer.id]
-      );
+    this.handleDifferentContainerDrop(
+      previousContainer.id,
+      container.id,
+      previousIndex
+    );
+  }
+
+  private handleSameContainerDrop(
+    containerId: string,
+    previousIndex: number,
+    currentIndex: number
+  ) {
+    const handlers = {
+      [this.inTrainingListId]: () =>
+        moveItemInArray(
+          this.globalState.playerDataAcessor.inTrainingDigimonList,
+          previousIndex,
+          currentIndex
+        ),
+      [this.bitFarmingListId]: () =>
+        moveItemInArray(
+          this.globalState.playerDataAcessor.bitFarmDigimonList,
+          previousIndex,
+          currentIndex
+        ),
+    };
+
+    const handler = handlers[containerId];
+    if (handler) {
+      handler();
+    }
+  }
+
+  private handleDifferentContainerDrop(
+    previousContainerId: string,
+    containerId: string,
+    previousIndex: number
+  ) {
+    const digimon = this.getDigimonFromPreviousContainer(
+      previousContainerId,
+      previousIndex
+    );
+    const action = this.actions[previousContainerId];
+
+    const handlers = {
+      [this.inTrainingListId]: () =>
+        this.globalState.addDigimonToFarm(digimon, action),
+      [this.bitFarmingListId]: () =>
+        this.globalState.addDigimonToTraining(digimon, action),
+      [this.teamListId]: () =>
+        this.handleTeamListDrop(containerId, digimon, action),
+    };
+
+    const handler = handlers[previousContainerId];
+    if (handler) {
+      handler();
+    }
+  }
+
+  private handleTeamListDrop(
+    containerId: string,
+    digimon: Digimon,
+    action: any
+  ) {
+    const handlers = {
+      [this.inTrainingListId]: () =>
+        this.globalState.addDigimonToTraining(digimon, action),
+      [this.bitFarmingListId]: () =>
+        this.globalState.addDigimonToFarm(digimon, action),
+    };
+
+    const handler = handlers[containerId];
+    if (handler) {
+      handler();
+    }
+  }
+
+  private getDigimonFromPreviousContainer(
+    containerId: string,
+    index: number
+  ): Digimon {
+    const lists = {
+      [this.inTrainingListId]:
+        this.globalState.playerDataAcessor.inTrainingDigimonList,
+      [this.bitFarmingListId]:
+        this.globalState.playerDataAcessor.bitFarmDigimonList,
+      [this.teamListId]: this.globalState.playerDataAcessor.digimonList,
+    };
+
+    const list = lists[containerId];
+    if (list) {
+      return list[index];
     }
 
-    if (event.previousContainer.id === this.bitFarmingListId) {
-      this.globalState.addDigimonToTraining(
-        event.previousContainer.data.bitFarmDigimonList[event.previousIndex],
-        this.actions[event.previousContainer.id]
-      );
-    }
-
-    if (event.previousContainer.id === this.teamListId) {
-      if (event.container.id === this.inTrainingListId) {
-        this.globalState.addDigimonToTraining(
-          event.previousContainer.data.digimonList[event.previousIndex],
-          this.actions[event.previousContainer.id]
-        );
-        return;
-      }
-      if (event.container.id === this.bitFarmingListId) {
-        this.globalState.addDigimonToFarm(
-          event.previousContainer.data.digimonList[event.previousIndex],
-          this.actions[event.previousContainer.id]
-        );
-        return;
-      }
-    }
+    throw new Error('Invalid container ID');
   }
 }

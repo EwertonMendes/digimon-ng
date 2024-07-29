5738469b3fe7b023fa9bbc8eee3e7f55f70567cf
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-07-29 20:54:19 -0300

    feat: makes toast to be stackable

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,7 +1,13 @@
 import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { Toast, ToastService } from './toast.service';
 import { CommonModule } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-toast',
@@ -29,12 +35,16 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class ToastComponent implements OnInit {
   toasts: Toast[] = [];
+  maxToasts: number = 5;
 
   toastService = inject(ToastService);
   changeDetectorRef = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
     this.toastService.toasts$.subscribe((toast) => {
+      if (this.toasts.length >= this.maxToasts) {
+        this.toasts.shift();
+      }
       this.toasts.push(toast);
       setTimeout(() => this.removeToast(0), 5000);
     });

eebecd65b4db80d1ed6c728f8e8062c81c4cbe76
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-08-04 22:27:12 -0300

    feat: adds desktop route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DesktopComponent } from './modules/desktop/desktop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'desktop',
+    pathMatch: 'full',
+  },
+  {
+    path: 'desktop',
+    component: DesktopComponent,
+  },
+
+  { path: '**', redirectTo: 'desktop' },
+];

e821bf2174050c21ade106a9622b929a7b2219fa
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-08-12 16:25:56 -0300

    feat: addsclick effect on some actions

--- src/app/common/action-bar/action-bar.component.ts
+++ src/app/common/action-bar/action-bar.component.ts
@@ -7,6 +7,8 @@ import { ModalService } from '../../shared/components/modal/modal.service';
 import { Digimon } from '../../core/interfaces/digimon.interface';
 import { DigimonListLocation } from '../../core/enums/digimon-list-location.enum';
 import { Router } from '@angular/router';
+import { AudioService } from '../../services/audio.service';
+import { AudioEffects } from '../../core/enums/audio-tracks.enum';
 
 @Component({
   selector: 'app-action-bar',
@@ -21,30 +23,42 @@ export class ActionBarComponent {
 
   globalState = inject(GlobalStateDataSource);
   modalService = inject(ModalService);
+  audioService = inject(AudioService);
   router = inject(Router);
 
   openDigimonStorageModal() {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.modalService.open(this.digimonStorageModalId);
   }
 
+  navigateTo(url: string) {
+    this.audioService.playAudio(AudioEffects.CLICK);
+    this.router.navigate([url]);
+  }
+
   openDigimonDetailsModal(digimon: Digimon) {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.globalState.setSelectedDigimonOnDetailsAccessor(digimon);
     this.modalService.open(this.digimonDetailsModalId);
   }
 
   addDigimonToTeam(digimon: Digimon) {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.globalState.addDigimonToList(digimon, DigimonListLocation.STORAGE);
   }
 
   addDigimonToTraining(digimon: Digimon) {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.globalState.addDigimonToTraining(digimon, DigimonListLocation.STORAGE);
   }
 
   addDigimonToFarm(digimon: Digimon) {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.globalState.addDigimonToFarm(digimon, DigimonListLocation.STORAGE);
   }
 
   addDigimonToHospital(digimon: Digimon) {
+    this.audioService.playAudio(AudioEffects.CLICK);
     this.globalState.addDigimonToHospital(digimon, DigimonListLocation.STORAGE);
   }
 }

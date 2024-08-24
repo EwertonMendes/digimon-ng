f05c34f979034a69274ebe0748f9725bc0ccf365
Ewerton Mendes <ewertonmendes9@gmail.com>
2024-08-24 19:53:05 -0300

    refactor: refactor evolution tree modal graph logic

---
+++ src/app/services/graph.service.ts
@@ -0,0 +1,160 @@
+import Graph from 'graphology';
+import Sigma from 'sigma';
+import { Injectable } from '@angular/core';
+import { createNodeImageProgram } from '@sigma/node-image';
+import { Settings } from 'sigma/settings';
+import { NodeDisplayData, PartialButFor } from 'sigma/types';
+import { Digimon } from '../core/interfaces/digimon.interface';
+import { DigimonService } from './digimon.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GraphService {
+  constructor(private digimonService: DigimonService) {}
+
+  createGraph(
+    container: HTMLElement,
+    mainDigimon: Digimon,
+    evolutionRouteDigimons?: Digimon[]
+  ): Sigma {
+    const graph = new Graph();
+
+    this.addEvolutionRouteNodes(graph, evolutionRouteDigimons);
+    this.addMainDigimonNode(graph, mainDigimon, evolutionRouteDigimons);
+    this.addPossibleEvolutions(graph, mainDigimon, evolutionRouteDigimons);
+
+    return new Sigma(graph, container, {
+      nodeProgramClasses: {
+        image: createNodeImageProgram({
+          objectFit: 'contain',
+          padding: 0.1,
+          keepWithinCircle: true,
+        }),
+      },
+      minCameraRatio: 0.5,
+      maxCameraRatio: 2,
+      defaultDrawNodeLabel: this.drawLabel,
+    });
+  }
+
+  private addEvolutionRouteNodes(
+    graph: Graph,
+    evolutionRouteDigimons?: Digimon[]
+  ) {
+    evolutionRouteDigimons?.forEach((digimon, index) => {
+      if (!digimon) return;
+
+      graph.addNode(digimon.seed, {
+        label: `${digimon.name} (${digimon.rank})`,
+        x: index,
+        y: 0,
+        size: 40,
+        color: '#C0E5C8',
+        type: 'image',
+        image: digimon.img,
+        seed: digimon.seed,
+        rank: digimon.rank,
+      });
+
+      if (index > 0) {
+        graph.addEdge(evolutionRouteDigimons[index - 1]?.seed, digimon.seed, {
+          size: 5,
+          color: '#C0E5C8',
+        });
+      }
+    });
+  }
+
+  private addMainDigimonNode(
+    graph: Graph,
+    mainDigimon: Digimon,
+    evolutionRouteDigimons?: Digimon[]
+  ) {
+    graph.addNode(mainDigimon.seed, {
+      label: `${mainDigimon.name} (${mainDigimon.rank})`,
+      x: evolutionRouteDigimons?.length ?? 0,
+      y: 0,
+      size: 40,
+      color: '#C0E5C8',
+      type: 'image',
+      image: mainDigimon.img,
+      seed: mainDigimon.seed,
+      rank: mainDigimon.rank,
+    });
+
+    if (evolutionRouteDigimons?.length) {
+      graph.addEdge(
+        evolutionRouteDigimons[evolutionRouteDigimons.length - 1]?.seed,
+        mainDigimon.seed,
+        {
+          size: 5,
+          color: '#C0E5C8',
+        }
+      );
+    }
+  }
+
+  private addPossibleEvolutions(
+    graph: Graph,
+    mainDigimon: Digimon,
+    evolutionRouteDigimons?: Digimon[]
+  ) {
+    const possibleEvolutions =
+      this.digimonService.getDigimonEvolutions(mainDigimon);
+
+    possibleEvolutions.forEach((evolution, index) => {
+      graph.addNode(evolution.seed, {
+        label: `${evolution.name} (${evolution.rank})`,
+        x: (evolutionRouteDigimons?.length ?? 0) + 1,
+        y: index,
+        size: 40,
+        color: '#D95D39',
+        type: 'image',
+        image: evolution.img,
+        seed: evolution.seed,
+        rank: evolution.rank,
+      });
+
+      graph.addEdge(mainDigimon.seed, evolution.seed, {
+        size: 2,
+        color: 'black',
+      });
+    });
+  }
+
+  private drawLabel(
+    context: CanvasRenderingContext2D,
+    data: PartialButFor<
+      NodeDisplayData,
+      'x' | 'y' | 'size' | 'label' | 'color'
+    >,
+    settings: Settings
+  ): void {
+    if (!data.label) return;
+
+    const size = settings.labelSize,
+      font = settings.labelFont,
+      weight = settings.labelWeight;
+
+    context.font = `${weight} ${size}px ${font}`;
+    const textWidth = context.measureText(data.label).width + 8;
+    const textHeight = size;
+
+    const rectWidth = textWidth + 4;
+    const rectHeight = textHeight + 10;
+
+    const xPosition = data.x - rectWidth / 2;
+    const yPosition = data.y + 50;
+
+    context.fillStyle = '#ffffffcc';
+    context.fillRect(xPosition, yPosition, rectWidth, rectHeight);
+
+    context.fillStyle = '#000';
+    context.fillText(
+      data.label,
+      xPosition + 4,
+      yPosition + rectHeight / 2 + size / 3
+    );
+  }
+}
--- src/app/shared/components/evolution-tree-modal/evolution-tree-modal.component.ts
+++ src/app/shared/components/evolution-tree-modal/evolution-tree-modal.component.ts
@@ -1,19 +1,16 @@
-import Graph from 'graphology';
-import Sigma from 'sigma';
 import { Component, inject, input, signal } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
 import { Digimon } from '../../../core/interfaces/digimon.interface';
-import { createNodeImageProgram } from '@sigma/node-image';
-import { Settings } from 'sigma/settings';
-import { NodeDisplayData, PartialButFor } from 'sigma/types';
 import { DigimonService } from '../../../services/digimon.service';
+import { GraphService } from '../../../services/graph.service';
+import Sigma from 'sigma';
 
 @Component({
   selector: 'app-evolution-tree-modal',
   standalone: true,
   imports: [ModalComponent],
   templateUrl: './evolution-tree-modal.component.html',
-  styleUrl: './evolution-tree-modal.component.scss',
+  styleUrls: ['./evolution-tree-modal.component.scss'],
 })
 export class EvolutionTreeModalComponent {
   evolutionTreeModalId = 'evolution-tree-modal';
@@ -25,82 +22,62 @@ export class EvolutionTreeModalComponent {
   selectedDigimon = signal<Digimon | undefined>(undefined);
 
   digimonService = inject(DigimonService);
+  graphService = inject(GraphService);
 
   onOpen() {
     const container = document.getElementById('evolutionTreeWrapper')!;
     if (!container) return;
 
-    const graph = new Graph();
+    this.evolutionRouteDigimons = this.getEvolutionRouteDigimons();
 
-    this.evolutionRouteDigimons = this.mainDigimon()
-      ?.currentEvolutionRoute?.map((evolution) => {
-        const digimon = this.digimonService.getBaseDigimonDataBySeed(
-          evolution.seed
-        );
-
-        if (!digimon) return;
-
-        return digimon;
-      })
-      .filter((digimon) => digimon !== undefined) as Digimon[];
-
-    this.evolutionRouteDigimons?.forEach((digimon, index) => {
-      if (!digimon) return;
-
-      graph.addNode(digimon.seed, {
-        label: `${digimon.name} (${digimon.rank})`,
-        x: index,
-        y: 0,
-        size: 40,
-        color: '#C0E5C8',
-        type: 'image',
-        image: digimon.img,
-        seed: digimon.seed,
-        rank: digimon.rank,
-      });
-
-      if (index > 0) {
-        graph.addEdge(
-          this.evolutionRouteDigimons[index - 1]?.seed,
-          digimon.seed,
-          {
-            size: 5,
-            color: '#C0E5C8',
-          }
-        );
-      }
-    });
-
-    graph.addNode(this.mainDigimon()?.seed, {
-      label: `${this.mainDigimon()?.name} (${this.mainDigimon()?.rank})`,
-      x: this.evolutionRouteDigimons?.length ?? 0,
-      y: 0,
-      size: 40,
-      color: '#C0E5C8',
-      type: 'image',
-      image: this.mainDigimon()?.img,
-      seed: this.mainDigimon()?.seed,
-      rank: this.mainDigimon()?.rank,
-    });
-
-    if (this.evolutionRouteDigimons?.length) {
-      graph.addEdge(
-        this.evolutionRouteDigimons[this.evolutionRouteDigimons.length - 1]
-          ?.seed,
-        this.mainDigimon()?.seed,
-        { size: 5, color: '#C0E5C8' }
-      );
-    }
-
-    const possibleEvolutions = this.digimonService.getDigimonEvolutions(
-      this.mainDigimon()
+    this.sigma = this.graphService.createGraph(
+      container,
+      this.mainDigimon()!,
+      this.evolutionRouteDigimons
     );
 
-    possibleEvolutions.forEach((evolution, index) => {
-      graph.addNode(evolution.seed, {
+    this.sigma.on('clickNode', (data) => this.handleNodeClick(data));
+  }
+
+  private getEvolutionRouteDigimons(): Digimon[] {
+    return this.mainDigimon()
+      ?.currentEvolutionRoute?.map((evolution) =>
+        this.digimonService.getBaseDigimonDataBySeed(evolution.seed)
+      )
+      .filter((digimon) => digimon !== undefined) as Digimon[];
+  }
+
+  private handleNodeClick(data: any) {
+    const clickedNode = this.sigma.getGraph().getNodeAttributes(data.node);
+
+    this.currentRank = clickedNode['rank'];
+
+    this.removeUpperRankNodes(clickedNode);
+
+    const digimon = this.digimonService.getBaseDigimonDataBySeed(
+      clickedNode['seed']
+    );
+    this.selectedDigimon.set(digimon);
+
+    const possibleEvolutions =
+      this.digimonService.getDigimonEvolutions(digimon);
+
+    possibleEvolutions.forEach((evolution: Digimon, index: number) => {
+      if (this.sigma.getGraph().findNode((node) => node === evolution.seed)) {
+        return;
+      }
+
+      let newNodeX = clickedNode['x'] + 1;
+      let newNodeY = clickedNode['y'] + index;
+
+      if (this.hasNodeInPosition(newNodeX, newNodeY)) {
+        newNodeY++;
+      }
+
+      this.sigma.getGraph().addNode(evolution.seed, {
         label: `${evolution.name} (${evolution.rank})`,
-        x: this.evolutionRouteDigimons?.length! + 1,
-        y: index,
+        x: newNodeX,
+        y: newNodeY,
         size: 40,
         color: '#D95D39',
         type: 'image',
@@ -108,120 +85,31 @@ export class EvolutionTreeModalComponent {
         seed: evolution.seed,
         rank: evolution.rank,
       });
-
-      graph.addEdge(this.mainDigimon()?.seed, evolution.seed, {
+      this.sigma.getGraph().addEdge(clickedNode['seed'], evolution.seed, {
         size: 2,
         color: 'black',
       });
     });
+  }
 
-    this.currentRank = this.mainDigimon()?.rank ?? 'Fresh';
-
-    this.sigma = new Sigma(graph, container, {
-      nodeProgramClasses: {
-        image: createNodeImageProgram({
-          objectFit: 'contain',
-          padding: 0.1,
-          keepWithinCircle: true,
-        }),
-      },
-      minCameraRatio: 0.5,
-      maxCameraRatio: 2,
-      defaultDrawNodeLabel: function drawLabel(
-        context: CanvasRenderingContext2D,
-        data: PartialButFor<
-          NodeDisplayData,
-          'x' | 'y' | 'size' | 'label' | 'color'
-        >,
-        settings: Settings
-      ): void {
-        if (!data.label) return;
-
-        const size = settings.labelSize,
-          font = settings.labelFont,
-          weight = settings.labelWeight;
-
-        context.font = `${weight} ${size}px ${font}`;
-        const textWidth = context.measureText(data.label).width + 8;
-        const textHeight = size;
-
-        const rectWidth = textWidth + 4;
-        const rectHeight = textHeight + 10;
-
-        const xPosition = data.x - rectWidth / 2;
-        const yPosition = data.y + 50;
-
-        context.fillStyle = '#ffffffcc';
-        context.fillRect(xPosition, yPosition, rectWidth, rectHeight);
-
-        context.fillStyle = '#000';
-        context.fillText(
-          data.label,
-          xPosition + 4,
-          yPosition + rectHeight / 2 + size / 3
+  private removeUpperRankNodes(clickedNode: any) {
+    const upperRankNodeToDrop = this.sigma
+      .getGraph()
+      .nodes()
+      .filter((node) => {
+        const nodeAttributes = this.sigma.getGraph().getNodeAttributes(node);
+        return (
+          nodeAttributes['seed'] !== this.mainDigimon()?.seed &&
+          !this.evolutionRouteDigimons.find(
+            (evolution) => evolution.seed === nodeAttributes['seed']
+          ) &&
+          this.digimonService.getRankOrder(nodeAttributes['rank']) >
+            this.digimonService.getRankOrder(this.currentRank)
         );
-      },
-    });
-
-    this.sigma.on('clickNode', (data) => {
-      const clickedNode = this.sigma.getGraph().getNodeAttributes(data.node);
-
-      this.currentRank = clickedNode['rank'];
-
-      const upperRankNodeToDrop = this.sigma
-        .getGraph()
-        .nodes()
-        .filter((node) => {
-          const nodeAttributes = this.sigma.getGraph().getNodeAttributes(node);
-          return (
-            nodeAttributes['seed'] !== this.mainDigimon()?.seed &&
-            !this.evolutionRouteDigimons.find(
-              (evolution) => evolution.seed === nodeAttributes['seed']
-            ) &&
-            this.digimonService.getRankOrder(nodeAttributes['rank']) >
-              this.digimonService.getRankOrder(this.currentRank)
-          );
-        });
-
-      upperRankNodeToDrop.forEach((node) => {
-        this.sigma.getGraph().dropNode(node);
       });
 
-      const digimon = this.digimonService.getBaseDigimonDataBySeed(
-        clickedNode['seed']
-      );
-      this.selectedDigimon.set(digimon);
-      const possibleEvolutions =
-        this.digimonService.getDigimonEvolutions(digimon);
-
-      possibleEvolutions.forEach((evolution: Digimon, index: number) => {
-        if (this.sigma.getGraph().findNode((node) => node === evolution.seed)) {
-          return;
-        }
-
-        let newNodeX = clickedNode['x'] + 1;
-        let newNodeY = clickedNode['y'] + index;
-
-        if (this.hasNodeInPosition(newNodeX, newNodeY)) {
-          newNodeY++;
-        }
-
-        this.sigma.getGraph().addNode(evolution.seed, {
-          label: `${evolution.name} (${evolution.rank})`,
-          x: newNodeX,
-          y: newNodeY,
-          size: 40,
-          color: '#D95D39',
-          type: 'image',
-          image: evolution.img,
-          seed: evolution.seed,
-          rank: evolution.rank,
-        });
-        this.sigma.getGraph().addEdge(clickedNode['seed'], evolution.seed, {
-          size: 2,
-          color: 'black',
-        });
-      });
+    upperRankNodeToDrop.forEach((node) => {
+      this.sigma.getGraph().dropNode(node);
     });
   }
 

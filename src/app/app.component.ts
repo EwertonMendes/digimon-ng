228510fd7d0983ba57b01d9ebf1cb6afa993476e
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-04-19 20:58:33 -0300

    chore: makes layout scale to the proper resolution automatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GlobalStateDataSource } from './state/global-state.datasource';
 import { ActionBarComponent } from './common/action-bar/action-bar.component';
@@ -25,12 +25,32 @@ export class AppComponent implements OnInit {
   globalState = inject(GlobalStateDataSource);
   playerDataService = inject(PlayerDataService);
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.adjustZoom();
+  }
+
   async ngOnInit() {
+    this.adjustZoom();
     const playerData = await this.playerDataService.loadPlayerData();
     if (!playerData) {
       this.globalState.showInitialSetupScreen.set(true);
       return;
     }
     this.globalState.initializeGame(playerData);
+  }
+
+  private adjustZoom(): void {
+    const baseWidth = 1920;
+    const baseHeight = 1080;
+    const currentWidth = window.innerWidth;
+    const currentHeight = window.innerHeight;
+
+    const zoom = Math.max(currentWidth / baseWidth, currentHeight / baseHeight);
+
+    const appRoot = document.querySelector('app-root') as HTMLElement;
+    if (appRoot) {
+      appRoot.style.zoom = `${zoom}`;
+    }
   }
 }

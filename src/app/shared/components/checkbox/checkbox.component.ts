a9e81cd05028c943fceeaf165e1a94e1522f6de3
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-07-04 22:23:59 -0300

    feat: implement custom checkbox component with toggle functionality

---
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -0,0 +1,49 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
+})
+export class CheckboxComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() disabled: boolean = false;
+
+  value: boolean = false;
+
+  onChange = (value: any) => { };
+  onTouched = () => { };
+
+  writeValue(obj: any): void {
+    this.value = !!obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  toggle(): void {
+    if (this.disabled) return;
+    this.value = !this.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+}

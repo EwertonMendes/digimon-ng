d4c05790b6189de59c41c6bfd0bb722cc00b9fd0
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-08-12 23:31:28 -0300

    feat: add player info modal translations

--- src/app/shared/components/player-info-modal/player-info-modal.component.ts
+++ src/app/shared/components/player-info-modal/player-info-modal.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, inject } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
 import { GlobalStateDataSource } from '../../../state/global-state.datasource';
 import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-player-info-modal',
   standalone: true,
-  imports: [CommonModule, ModalComponent],
+  imports: [CommonModule, ModalComponent, TranslocoModule],
   templateUrl: './player-info-modal.component.html',
   styleUrl: './player-info-modal.component.scss',
 })

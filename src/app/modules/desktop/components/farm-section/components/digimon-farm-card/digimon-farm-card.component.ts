1258f2a861969201b9c8c726b75d38a21c8d83fe
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-08-23 21:37:49 -0300

    style: makes digimon farm card show when digimon is fainted

--- src/app/modules/desktop/components/farm-section/components/digimon-farm-card/digimon-farm-card.component.ts
+++ src/app/modules/desktop/components/farm-section/components/digimon-farm-card/digimon-farm-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, input } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { Digimon } from '@core/interfaces/digimon.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-digimon-farm-card',
   standalone: true,
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, CommonModule],
   templateUrl: './digimon-farm-card.component.html',
   styleUrl: './digimon-farm-card.component.scss'
 })
 export class DigimonFarmCardComponent {
-  digimon = input<Digimon>();
+  digimon = input.required<Digimon>();
 }

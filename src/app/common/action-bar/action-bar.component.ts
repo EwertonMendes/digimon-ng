852e75e6a495c0c789df5f14ba4a933e8913521d
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-08-26 22:54:53 -0300

    chore: replace action bar icon with animated svg icons

--- src/app/common/action-bar/action-bar.component.ts
+++ src/app/common/action-bar/action-bar.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { ButtonComponent } from '@shared/components/button/button.component';
-import { Router } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit, signal, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AudioService } from '@services/audio.service';
 import { AudioEffects } from '@core/enums/audio-tracks.enum';
 import { StorageModalComponent } from '@shared/components/storage-modal/storage-modal.component';
@@ -13,31 +12,49 @@ import { ConfigModalComponent } from './config-modal/config-modal.component';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 import { TooltipDirective } from 'app/directives/tooltip.directive';
 import { ModalService } from 'app/shared/components/modal/modal.service';
+import { IconComponent } from "@shared/components/icon/icon.component";
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-action-bar',
   standalone: true,
   imports: [
-    ButtonComponent,
     TranslocoModule,
-    TooltipDirective
+    TooltipDirective,
+    IconComponent,
+    CommonModule
   ],
   templateUrl: './action-bar.component.html',
   styleUrl: './action-bar.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class ActionBarComponent {
+export class ActionBarComponent implements OnInit {
   playerInfoModalId = 'player-info-modal';
   digimonStorageModalId = 'digimon-storage-modal';
   debugModalId = 'debug-modal';
   configModalId = 'config-modal';
   isDevMode = !environment.production;
 
-  modalService = inject(ModalService);
-  audioService = inject(AudioService);
-  router = inject(Router);
-  globalState = inject(GlobalStateDataSource);
-  toastService = inject(ToastService);
-  translocoService = inject(TranslocoService);
+  protected currentRoute = signal<string>('');
+
+  private modalService = inject(ModalService);
+  private audioService = inject(AudioService);
+  private router = inject(Router);
+  protected globalState = inject(GlobalStateDataSource);
+  private toastService = inject(ToastService);
+  private translocoService = inject(TranslocoService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute.set(event.urlAfterRedirects.split('/')[1]);
+    });
+  }
 
   openDigimonStorageModal() {
     this.audioService.playAudio(AudioEffects.CLICK);
---
+++ src/app/shared/components/icon/icon.component.ts
@@ -0,0 +1,80 @@
+import { Component, DestroyRef, effect, inject, input, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { catchError, EMPTY, map } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div [innerHTML]="svgContent() || fallbackSvg" [ngStyle]="{'width': size(), 'height': size(), 'display': 'block', 'vertical-align': 'middle', 'margin': '0', 'padding': '0', 'overflow': 'visible', 'box-sizing': 'border-box', 'position': 'relative'}" [attr.aria-label]="ariaLabel()"></div>
+  `,
+  styles: `
+    :host {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      overflow: visible;
+    }
+    div {
+      line-height: 0;
+    }
+    div > svg {
+      width: 100%;
+      height: 100%;
+    }
+  `
+})
+export class IconComponent {
+  name = input.required<string>();
+  size = input<string>('24px');
+  color = input<string>('currentColor');
+  ariaLabel = input<string>('icon');
+
+  svgContent = signal<SafeHtml>('');
+  protected fallbackSvg: SafeHtml;
+
+  private static iconCache: { [key: string]: SafeHtml } = {};
+
+  private http = inject(HttpClient);
+  private sanitizer = inject(DomSanitizer);
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    this.fallbackSvg = this.sanitizer.bypassSecurityTrustHtml(
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect fill="gray" x="2" y="2" width="20" height="20"/></svg>'
+    );
+
+    effect(() => {
+      const iconName = this.name();
+      if (iconName) {
+        this.loadSvg(iconName);
+      }
+    });
+  }
+
+  private loadSvg(iconName: string) {
+    if (IconComponent.iconCache[iconName]) {
+      this.svgContent.set(IconComponent.iconCache[iconName]);
+    } else {
+      this.http
+        .get(`assets/icons/${iconName}.svg`, { responseType: 'text' })
+        .pipe(takeUntilDestroyed(this.destroyRef),
+          map(svg => {
+            const safeSvg = this.sanitizer.bypassSecurityTrustHtml(svg);
+            IconComponent.iconCache[iconName] = safeSvg;
+            return safeSvg;
+          }),
+          catchError(() => {
+            return EMPTY;
+          }),
+        )
+        .subscribe(safeSvg => {
+          this.svgContent.set(safeSvg);
+        });
+    }
+  }
+}

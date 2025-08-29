034ff4d3a885684e49fa870224d7b6c531cebdc2
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-08-28 23:12:35 -0300

    chore: updates locations digimons

--- src/app/modules/adventure/components/explore-section/explore-section.component.ts
+++ src/app/modules/adventure/components/explore-section/explore-section.component.ts
@@ -4,15 +4,15 @@ import { GlobalStateDataSource } from '@state/global-state.datasource';
 import { BattleModalComponent } from '@shared/components/battle-modal/battle-modal.component';
 import { Digimon } from '@core/interfaces/digimon.interface';
 import { ToastService } from '@shared/components/toast/toast.service';
-import { DigimonSeeds } from '@core/enums/digimon-seeds.enum';
 import { AudioService } from '@services/audio.service';
 import { AudioEffects } from '@core/enums/audio-tracks.enum';
 import { ModalService } from 'app/shared/components/modal/modal.service';
+import { LOCATIONS } from '@core/consts/locations';
 
 interface Location {
   name: string;
   img: string;
-  possibleEcounterDigimonSeeds: string[];
+  possibleEcounterDigimonSeeds: { seed: string; levelRange: { min: number; max: number } }[];
   levelRange: { min: number; max: number };
 }
 
@@ -30,250 +30,7 @@ export class ExploreSectionComponent {
   private toastService = inject(ToastService);
   private audioService = inject(AudioService);
 
-  locations: Location[] = [
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_LOGIN_MOUNTAIN'),
-      img: 'assets/environments/loginmountain.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.BOTAMON,
-        DigimonSeeds.KOROMON,
-        DigimonSeeds.PITCHMON,
-        DigimonSeeds.PUTTIMON,
-        DigimonSeeds.MINOMON,
-        DigimonSeeds.AGUMON,
-        DigimonSeeds.GREYMON,
-        DigimonSeeds.FALCOMON,
-        DigimonSeeds.GOBURIMON,
-      ],
-      levelRange: { min: 1, max: 10 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_PIXEL_DESERT'),
-      img: 'assets/environments/pixeldesert.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.KOROMON,
-        DigimonSeeds.AGUMON,
-        DigimonSeeds.GEO_GREYMON,
-        DigimonSeeds.APEMON,
-        DigimonSeeds.SKULL_GREYMON,
-        DigimonSeeds.METAL_GREYMON,
-        DigimonSeeds.GROWLMON,
-        DigimonSeeds.KUNEMON,
-        DigimonSeeds.GABUMON,
-        DigimonSeeds.GUILMON,
-      ],
-      levelRange: { min: 5, max: 15 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_LABEL_FOREST'),
-      img: 'assets/environments/labelforest.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.TENTOMON,
-        DigimonSeeds.KABUTERIMON,
-        DigimonSeeds.MEGA_KABUTERIMON_RED,
-        DigimonSeeds.MEGA_KABUTERIMON_BLUE,
-        DigimonSeeds.KUNEMON,
-        DigimonSeeds.MINOMON,
-        DigimonSeeds.GATOMON,
-        DigimonSeeds.SALAMON,
-        DigimonSeeds.DOKUGUMON,
-      ],
-      levelRange: { min: 10, max: 20 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_REGISTER_JUNGLE'),
-      img: 'assets/environments/registerjungle.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.GABUMON,
-        DigimonSeeds.GARURUMON,
-        DigimonSeeds.WAR_GARURUMON,
-        DigimonSeeds.METAL_GARURUMON,
-        DigimonSeeds.MINOMON,
-        DigimonSeeds.GOBURIMON,
-        DigimonSeeds.DODOMON,
-        DigimonSeeds.FUGAMON,
-        DigimonSeeds.OGREMON,
-      ],
-      levelRange: { min: 15, max: 25 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_PROXY_ISLAND'),
-      img: 'assets/environments/proxyisland.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.GOMAMON,
-        DigimonSeeds.IKKAKUMON,
-        DigimonSeeds.GIGIMON,
-        DigimonSeeds.GUILMON,
-        DigimonSeeds.GROWLMON,
-        DigimonSeeds.WAR_GROWLMON,
-        DigimonSeeds.GEO_GREYMON,
-      ],
-      levelRange: { min: 20, max: 30 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_LIMIT_VALLEY'),
-      img: 'assets/environments/limitvalley.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.AGUMON,
-        DigimonSeeds.GREYMON,
-        DigimonSeeds.GEO_GREYMON,
-        DigimonSeeds.RIZE_GREYMON,
-        DigimonSeeds.SHINE_GREYMON,
-        DigimonSeeds.BLUE_GREYMON,
-        DigimonSeeds.DOKUGUMON,
-        DigimonSeeds.KABUTERIMON,
-        DigimonSeeds.MEGA_KABUTERIMON_BLUE,
-        DigimonSeeds.MEGA_KABUTERIMON_RED,
-      ],
-      levelRange: { min: 25, max: 35 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_LOOP_SWAMP'),
-      img: 'assets/environments/loopswamp.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.BETAMON,
-        DigimonSeeds.SEADRAMON,
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.GOMAMON,
-        DigimonSeeds.IKKAKUMON,
-        DigimonSeeds.GOMAMON,
-        DigimonSeeds.IKKAKUMON,
-        DigimonSeeds.ZUDOMON,
-        DigimonSeeds.KYUKIMON,
-      ],
-      levelRange: { min: 30, max: 40 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_ACCESS_GLACIER'),
-      img: 'assets/environments/accessglacier.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.GABUMON,
-        DigimonSeeds.GARURUMON,
-        DigimonSeeds.WAR_GARURUMON,
-        DigimonSeeds.METAL_GARURUMON,
-        DigimonSeeds.ZUDOMON,
-        DigimonSeeds.VIKEMON,
-      ],
-      levelRange: { min: 35, max: 45 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_ANGLER_TUNNEL'),
-      img: 'assets/environments/anglertunnel.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.KOKUWAMON,
-        DigimonSeeds.BLUE_METAL_GREYMON,
-        DigimonSeeds.SHINE_GREYMON,
-        DigimonSeeds.SKULL_GREYMON,
-        DigimonSeeds.MACHINEDRAMON,
-      ],
-      levelRange: { min: 40, max: 50 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_MAGNET_MINE'),
-      img: 'assets/environments/magnetmine.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.RIZE_GREYMON,
-        DigimonSeeds.METAL_GREYMON,
-        DigimonSeeds.WAR_GREYMON,
-        DigimonSeeds.SKULL_GREYMON,
-        DigimonSeeds.MACHINEDRAMON,
-      ],
-      levelRange: { min: 45, max: 55 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_PACKET_COAST'),
-      img: 'assets/environments/packetcoast.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.TSUNOMON,
-        DigimonSeeds.ARMADILLOMON,
-        DigimonSeeds.GOMAMON,
-        DigimonSeeds.IKKAKUMON,
-        DigimonSeeds.GREYMON,
-        DigimonSeeds.GEO_GREYMON,
-        DigimonSeeds.RIZE_GREYMON,
-        DigimonSeeds.SHINE_GREYMON,
-      ],
-      levelRange: { min: 50, max: 60 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_PALLETE_AMAZON'),
-      img: 'assets/environments/paletteamazon.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.BETAMON,
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.OGREMON,
-        DigimonSeeds.KOROMON,
-        DigimonSeeds.IKKAKUMON,
-        DigimonSeeds.KABUTERIMON,
-        DigimonSeeds.MEGA_KABUTERIMON_BLUE,
-        DigimonSeeds.MEGA_KABUTERIMON_RED,
-        DigimonSeeds.HERCULES_KABUTERIMON,
-      ],
-      levelRange: { min: 55, max: 65 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_THRILLER_RUINS'),
-      img: 'assets/environments/thrillerruins.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.KUZUHAMON,
-        DigimonSeeds.BLACK_WAR_GREYMON,
-        DigimonSeeds.MEGIDRAMON,
-        DigimonSeeds.DOKUGUMON,
-        DigimonSeeds.KOKUWAMON,
-      ],
-      levelRange: { min: 60, max: 70 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_RISK_FACTORY'),
-      img: 'assets/environments/riskfactory.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.AIRDRAMON,
-        DigimonSeeds.METAL_GREYMON,
-        DigimonSeeds.BLUE_METAL_GREYMON,
-        DigimonSeeds.WAR_GREYMON,
-        DigimonSeeds.SKULL_GREYMON,
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.DOKUGUMON,
-        DigimonSeeds.KOKUWAMON,
-      ],
-      levelRange: { min: 65, max: 75 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_SHADOW_ABYSS'),
-      img: 'assets/environments/shadowabyss.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.BETAMON,
-        DigimonSeeds.SEADRAMON,
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.MEGIDRAMON,
-        DigimonSeeds.AIRDRAMON,
-        DigimonSeeds.GUILMON,
-        DigimonSeeds.GROWLMON,
-        DigimonSeeds.WAR_GROWLMON,
-        DigimonSeeds.GEO_GREYMON,
-        DigimonSeeds.ZEKE_GREYMON
-      ],
-      levelRange: { min: 70, max: 85 },
-    },
-    {
-      name: this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.LOCATION_WIZARD_TEMPLE'),
-      img: 'assets/environments/wizardtemple.png',
-      possibleEcounterDigimonSeeds: [
-        DigimonSeeds.ANGEWOMON,
-        DigimonSeeds.OPHANIMON,
-        DigimonSeeds.HOLYDRAMON,
-        DigimonSeeds.KYUKIMON,
-        DigimonSeeds.KUZUHAMON,
-        DigimonSeeds.DOKUGUMON,
-        DigimonSeeds.MEGIDRAMON,
-        DigimonSeeds.MACHINEDRAMON,
-        DigimonSeeds.BLACK_WAR_GREYMON,
-        DigimonSeeds.SKULL_GREYMON
-      ],
-      levelRange: { min: 75, max: 100 },
-    },
-  ];
+  locations: Location[] = [...LOCATIONS];
   currentLocation = signal<Location | null>(null)
 
   baseTurnOrder: Digimon[] = [];
@@ -313,12 +70,8 @@ export class ExploreSectionComponent {
       location.possibleEcounterDigimonSeeds.length === 0
     ) {
       for (let i = 0; i < randomNumber; i++) {
-        const randomLevel = Math.floor(
-          Math.random() * (location.levelRange.max - location.levelRange.min) +
-          location.levelRange.min
-        );
-        const opponentDigimon =
-          this.globalState.generateRandomDigimon(randomLevel);
+        const randomLevel = Math.floor(Math.random() * (10 - 1) + 1);
+        const opponentDigimon = this.globalState.generateRandomDigimon(randomLevel);
         this.globalState.enemyTeamAccessor.push(opponentDigimon);
         this.log(this.translocoService.translate('MODULES.ADVENTURE.EXPLORE_SECTION.ENEMY_FOUND', { name: opponentDigimon.name }));
       }
@@ -326,15 +79,16 @@ export class ExploreSectionComponent {
     }
 
     for (let i = 0; i < randomNumber; i++) {
-      const randomLevel = Math.floor(
-        Math.random() * (location.levelRange.max - location.levelRange.min) +
-        location.levelRange.min
-      );
       const randomPossibleSeedIndex = Math.floor(
         Math.random() * location.possibleEcounterDigimonSeeds.length
       );
+      const selectedDigimon = location.possibleEcounterDigimonSeeds[randomPossibleSeedIndex];
+      const randomLevel = Math.floor(
+        Math.random() * (selectedDigimon.levelRange.max - selectedDigimon.levelRange.min) +
+        selectedDigimon.levelRange.min
+      );
       const opponentDigimon = this.globalState.generateDigimonBySeed(
-        location.possibleEcounterDigimonSeeds[randomPossibleSeedIndex],
+        selectedDigimon.seed,
         randomLevel
       );
       if (!opponentDigimon) return;

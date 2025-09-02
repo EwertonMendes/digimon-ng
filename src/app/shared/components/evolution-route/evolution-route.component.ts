807fbb7248bfc039b03fbeffcc7eeb7057922a03
Ewerton Mendes <ewertonmendes9@gmail.com>
2025-09-02 20:28:39 -0300

    style: improves digimon details layout

--- src/app/shared/components/evolution-route/evolution-route.component.ts
+++ src/app/shared/components/evolution-route/evolution-route.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { BaseDigimon } from '@core/interfaces/digimon.interface';
+import { TooltipDirective } from "app/directives/tooltip.directive";
 
 @Component({
   selector: 'app-evolution-route',
   standalone: true,
-  imports: [],
+  imports: [TooltipDirective],
   templateUrl: './evolution-route.component.html',
   styleUrl: './evolution-route.component.scss',
 })
